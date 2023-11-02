43bddb997f5ae605dad56e23655d0032501a31cf
marlon99bit <seupodese200@gmail.com>
2023-11-02 19:37:52 -0300

    test

---
+++ views/cadastro.php
@@ -0,0 +1,37 @@
+<?php
+$banco = fopen("banco.csv", "r");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/welcome_page.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php while (($linha = fgetcsv($banco)) !== false) : ?>
+
+  <?php endwhile ?>
+
+  <div id="Formulario">
+    <h2>Adicionar</h2>
+    <form class="formes" action="adicionar.php" method="POST">
+      <div>
+        <label>Nome:</label>
+        <input name="nome" type="text" placeholder="Escreva seu nome...">
+      </div>
+      <div>
+        <label>idade:</label>
+        <input name="idade" type="number" placeholder="Coloque sua idade...">
+      </div>
+      <button>Adicionar</button>
+
+    </form>
+  </div>
+</body>
+
+</html>
