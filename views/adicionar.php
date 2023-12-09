1b330528b63149e05b7c1711e1613aa6f7a12002
Guig10 <147162574+Guig10@users.noreply.github.com>
2023-12-09 02:33:39 -0300

    Update adicionar.php
    add o include para a conexao com o banco de dados e a confirmaçao de senha

--- views/adicionar.php
+++ views/adicionar.php
@@ -1,48 +1,47 @@
 <?php
-// Conecte-se ao banco de dados (substitua as informações do banco de dados conforme necessário)
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "projetoqr";
+include 'conexao.php';
 
-//caso não funcione use o workbench
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+        // Recupere os dados do formulário
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $idade = $_POST['idade'];
+        $senha = $_POST['senha'];
+        $confirma_senha = $_POST['confirma_senha'];
 
-try {
-    // Crie a conexão PDO
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $senha);
-    
-    // Defina o modo de erro do PDO como exceção
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // Recupere os dados do formulário
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $idade = $_POST['idade']; 
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Use password_hash() para criptografar a senha
-    
-    // Prepare a consulta SQL com valores de espaço reservado
-    $sql = "INSERT INTO cadastro (nome, email, idade, senha) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    
-    // Vincule os valores dos parâmetros
-    $stmt->bindParam(1, $nome);
-    $stmt->bindParam(2, $email);
-    $stmt->bindParam(3, $idade);
-    $stmt->bindParam(4, $senha);
-    
-    // Execute a consulta
-    $stmt->execute();
-    
-    // Armazene a mensagem de sucesso em uma sessão
-    session_start();
-    $_SESSION['success_message'] = "Registro inserido com sucesso!";
-    
-    // Redirecione para /ProjetoQR/views/index.php
-    header("Location: /ProjetoQR/views/index.php");
-    exit(); // Certifique-se de sair após o redirecionamento para evitar execução adicional do código
-} catch (PDOException $e) {
-    echo "Erro ao inserir registro: " . $e->getMessage();
+        // Verifique se as senhas coincidem
+        if ($senha != $confirma_senha) {
+            throw new Exception("As senhas não coincidem. Por favor, tente novamente.");
+        }
+        
+
+        // Prepare a consulta SQL com valores de espaço reservado
+        $sql = "INSERT INTO cadastro (nome, email, idade, senha) VALUES (?, ?, ?, ?)";
+        $stmt = $conexao->prepare($sql);
+
+        // Vincule os valores dos parâmetros
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $email);
+        $stmt->bindParam(3, $idade);
+        $stmt->bindParam(4, $senha);
+
+        // Execute a consulta
+        $stmt->execute();
+
+        // Armazene a mensagem de sucesso em uma sessão
+        session_start();
+        $_SESSION['success_message'] = "Registro inserido com sucesso!";
+
+        // Redirecione para /ProjetoQR/views/index.php
+        header("Location: /ProjetoQR/views/index.php");
+        exit(); // Certifique-se de sair após o redirecionamento para evitar execução adicional do código
+    } catch (Exception $e) {
+        // Em caso de erro, redirecione de volta para a página de cadastro com uma mensagem de erro
+        session_start();
+        $_SESSION['error_message'] = $e->getMessage();
+        header("Location: /ProjetoQR/views/cadastro.php");
+        exit();
+    }
 }
-
-
 ?>
