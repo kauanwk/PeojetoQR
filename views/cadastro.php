744d724ca0db518efb7c5dfee0517a544fc13508
Kauã Felix <kfc1@discente.ifpe.edu.br>
2023-12-18 22:29:07 -0300

    Correções

--- views/cadastro.php
+++ views/cadastro.php
@@ -1,45 +1,69 @@
+<?php
+session_start();
+if (isset($_SESSION['success_message'])) {
+  echo '<p class="success">' . $_SESSION['success_message'] . '</p>';
+  unset($_SESSION['success_message']);
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gerador de QR Code</title>
+  <link rel="stylesheet" href="../css/cadastro.css">
+  <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+  <title>Cadastro</title>
 </head>
 
 <body>
-
-  <form>
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" placeholder="Digite seu nome" required>
-
-    <label for="idade">Idade:</label>
-    <input type="number" id="idade" placeholder="Digite sua idade" required>
-
-    <label for="email">E-mail:</label>
-    <input type="email" id="email" placeholder="Digite seu e-mail" required>
-
-    <button type="button" onclick="gerarqr()">Gerar QR Code</button>
-  </form>
-
-  <img id="QRcodeimag" alt="QR Code" />
-
-  <script>
-  function gerarqr() {
-    var nome = document.getElementById("nome").value;
-    var idade = document.getElementById("idade").value;
-    var email = document.getElementById("email").value;
-    var inputData = "Nome: " + nome + ", Idade: " + idade + ", E-mail: " + email;
-    var GoogleChartAPI =
-      "https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=";
-    var GoogleChartAPI = GoogleChartAPI + encodeURI(inputData);
-    document.querySelector("#QRcodeimag").src = GoogleChartAPI;
-
-    console.log(GoogleChartAPI);
-  }
-  </script>
-
+  <div class="head">
+    <div class="head-img"> <img src="../images/logo.png" alt=""> </div>
+    <div class="head-link">
+      <h2 onclick="window.location='sobre.php'">Sobre</h2>
+      <h2 onclick="window.location='cadastro.php'">Cadastro</h2>
+      <h2 onclick="window.location='login.php'">Login</h2>
+    </div>
+  </div>
+  <div class="main">
+    <div class="div1">
+      <form action="adicionar.php" method="POST">
+        <div>
+          <h1>Cadastro</h1>
+        </div>
+        <div class="container-form">
+          <label for="nome">Nome:</label>
+          <input name="nome" type="text" id="nome" placeholder="Escreva seu nome completo...">
+        </div>
+        <div class="container-form">
+          <label for="idade">Idade:</label>
+          <input name="idade" id="idade" placeholder="Coloque sua idade...">
+        </div>
+        <div class="container-form">
+          <label for="email">E-mail:</label>
+          <input name="email" id="email" placeholder="Digite seu e-mail...">
+        </div>
+        <div class="container-form">
+          <label for="telefone">Telefone:</label>
+          <input name="telefone" id="telefone" type="text" placeholder="Digite seu telefone...">
+        </div>
+        <div class="container-form">
+          <label for="senha">Senha:</label>
+          <input name="senha" id="senha" type="password" placeholder="Digite sua senha...">
+        </div>
+        <div class="container-form">
+          <label for="confirma_senha">Confirme a Senha:</label>
+          <input name="confirma_senha" id="confirma_senha" type="password" placeholder="Confirme sua senha...">
+        </div>
+        <div class="button">
+          <button>Adicionar</button>
+        </div>
+      </form>
+    </div>
+    <div class="div21">
+      <img class="img1" src="../images/cadastro.png" alt="">
+    </div>
+  </div>
 </body>
 
 </html>
